e34c88251dc864a564ca79ad25412d3125e7850e
Frank Haugen <frank.haugen@semine.no>
2020-10-25 02:36:09 +0200

    Added some RoslyScripting things

---
+++ Frank.Apps.RuntimeExecution/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Frank.Apps.RuntimeExecution
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly CodeExecuter _codeExecuter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _codeExecuter = new CodeExecuter();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var code = Editor.Text;
+            var result = await _codeExecuter.RoslynScriptingAsync(code);
+            MessageBox.Show(result);
+        }
+    }
+}
